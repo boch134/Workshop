3968e0530b4f2bdef945273b209a5086dc78c6aa
Emine bouchoucha <62485054+boch134@users.noreply.github.com>
2021-02-21 14:16:40 +0100

    workshopv12

--- src/main/java/com/sip/workshop/controllers/loginController.java
+++ src/main/java/com/sip/workshop/controllers/loginController.java
@@ -44,22 +44,18 @@ public class loginController {
 	@RequestMapping(value = "/registration", method = RequestMethod.POST)
 	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
 		ModelAndView modelAndView = new ModelAndView();
-		if (user== null){
-			modelAndView.setViewName("registration");
-		}
+		if (user== null) modelAndView.setViewName("registration");
 		User userExists = userService.findUserByEmail(user.getEmail());
 		if (userExists != null) {
 			bindingResult.rejectValue("email", "error.user",
 					"There is already a user registered with the email provided");
 		}
-		if (bindingResult.hasErrors() ) {
-			modelAndView.setViewName("registration");
-		} else {
+		if (!bindingResult.hasErrors()) {
 			userService.saveUser(user);
 			modelAndView.addObject("successMessage", "User has been registered successfully");
 			modelAndView.addObject("user", new User());
-			modelAndView.setViewName("registration");
 		}
+		modelAndView.setViewName("registration");
 		return modelAndView;
 	}
 

0db2a751cd49c8e0623f697def44c4783cae3afe
Emine bouchoucha <62485054+boch134@users.noreply.github.com>
2021-02-21 12:28:41 +0100

    workshopv10

--- src/main/java/com/sip/workshop/configuration/DatabaseConfig.java
+++ src/main/java/com/sip/workshop/configuration/DatabaseConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
-    @Value("${postgres://zqoqoflaycoqqk:92618819428f02451929590f82d004be3f102d78ed1d4513a9b4ca14f4fc8872@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/devl8m4vh9gdbf}")
+    @Value("${spring.datasource.url}")
     private String dbUrl;
     @Bean
     public DataSource dataSource() {

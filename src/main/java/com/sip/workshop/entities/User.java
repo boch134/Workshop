9b9b73c5b2557b90ee1a828ac9df74d81477a6df
Emine bouchoucha <62485054+boch134@users.noreply.github.com>
2021-02-21 11:32:00 +0100

    workshopv3

--- src/main/java/com/sip/workshop/entities/User.java
+++ src/main/java/com/sip/workshop/entities/User.java
@@ -30,7 +30,7 @@ private String password;
 	@Column(name = "active")
 	private int active;
 	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "Utilisateur_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles;
 
 	public int getId() {
